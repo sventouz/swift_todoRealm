e0b19b6ce1c8be9023b465c598ca0f6109bc7319
sventouz <sventouz@gmail.com>
2020-01-02 10:28:40 +0900

    Initial Commit

---
+++ MyRealmTodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyRealmTodo
+//
+//  Created by higuchiryunosuke on 2020/01/02.
+//  Copyright © 2020年 higuchiryunosuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
