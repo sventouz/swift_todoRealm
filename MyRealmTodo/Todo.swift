b4899347e316ee864af987b296fca5942cc47681
sventouz <sventouz@gmail.com>
2020-01-02 12:51:19 +0900

    initial commit

---
+++ MyRealmTodo/Todo.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  MyRealmTodo
+//
+//  Created by higuchiryunosuke on 2020/01/02.
+//  Copyright © 2020年 higuchiryunosuke. All rights reserved.
+//
+
+import Foundation
---
+++ MyRealmTodo/addViewController.swift
@@ -0,0 +1,30 @@
+//
+//  addViewController.swift
+//  MyRealmTodo
+//
+//  Created by higuchiryunosuke on 2020/01/02.
+//  Copyright © 2020年 higuchiryunosuke. All rights reserved.
+//
+
+import UIKit
+
+class addViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
